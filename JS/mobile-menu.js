230c4a4f2283dd287e06ee6106bd626048aa6be5
HelenBoyko <strikazza@gmail.com>
2023-04-29 15:28:09 +0300

    header_adress-style

---
+++ JS/mobile-menu.js
@@ -0,0 +1,13 @@
+(() => {
+    const refs = {
+        openMenuBtn: document.querySelector('[data-mobile-menu-open]'),
+        closeMenuBtn: document.querySelector('[data-mobile-menu-close]'),
+        menu: document.querySelector('[data-mobile-menu]'),
+    };
+    refs.openMenuBtn.addEventListener('click', toggleMenu);
+    refs.closeMenuBtn.addEventListener('click', toggleMenu);
+    function toggleMenu() {
+        refs.menu.classList.toggle('is-hidden');
+        document.body.classList.toggle('no-scroll');
+    }
+})();
